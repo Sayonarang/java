73f5599e4b1a3e3c9f70cf9873c7e16f1095604f
Sayonara <102153239+Sayonarang@users.noreply.github.com>
2022-06-28 15:50:42 +0900

    Add files via upload

---
+++ fieldTest/FieldExample.java
@@ -0,0 +1,18 @@
+package fieldTest;
+
+public class FieldExample {
+
+	public static void main(String[] args) {
+		FieldValue fiv = new FieldValue();
+		System.out.println(fiv.bytefield);
+		System.out.println("byre입 필드 선언:"+fiv.bytefield);
+		System.out.println("char타입 필드 선언:"+fiv.charfield);
+		System.out.println(" short입 필드 선언:"+fiv.shortfield);
+		System.out.println(" float입 필드 선언:"+fiv.floatfield);
+		System.out.println(" boolean입 필드 선언:"+fiv.booleanfield);
+		System.out.println(" 배열 타입 필드 선언:"+fiv.arrfield);
+		
+		
+	}
+
+}
---
+++ fieldTest/FieldValue.java
@@ -0,0 +1,19 @@
+package fieldTest;
+
+public class FieldValue {
+	//필드
+	byte bytefield;
+	char charfield;
+	short shortfield;
+	float floatfield = 3.14f;
+	boolean booleanfield;
+	
+	int[] arrfield;
+	
+	//생성자  생략 가능 
+	
+	
+	//메소드
+	
+	
+}
