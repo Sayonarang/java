b266347d49dc1d5d3428391cec916955a53dce3c
Sayonara <102153239+Sayonarang@users.noreply.github.com>
2022-07-19 13:48:43 +1000

    Add files via upload

---
+++ LessonTest/src/Frame.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+//import javax.swing.JPanel;
+
+public class Frame {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		
+		frame.setVisible(true);
+		
+		//JPanel panel = new JPanel();
+		
+		
+	}
+
+}
