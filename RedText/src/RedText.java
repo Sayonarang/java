0e891ae84d7c9e936409272551ab38c09f37eb38
Sayonara <102153239+Sayonarang@users.noreply.github.com>
2022-07-21 01:00:30 +0900

    Add files via upload
    GUI

--- RedText/src/RedText.java
+++ RedText/src/RedText.java
@@ -1,8 +1,7 @@
-
-
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -15,27 +14,44 @@ class ImagePanel extends JPanel{
 		setSize(new Dimension(img.getWidth(null),img.getHeight(null)));
 		setPreferredSize(new Dimension(img.getWidth(null),img.getHeight(null)));
 		setLayout(null);
+		
 	}
-	//이미지를 불러옴
-	public void panintComponent(Graphics g) { //pain 어떤 이미지를 자동으로 비추어짐
+	//이미지를 불러옴 //pain 어떤 이미지를 자동으로 비추어짐
+	public void paintComponent(Graphics g) { 
 		g.drawImage(img, 0, 0, null);
+		
 	}
 }
 
 public class RedText {
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("RedText ");
+		JFrame frame = new JFrame("RedText 0");
 		frame.setSize(640, 480);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 		
-		ImagePanel panel = new ImagePanel(new ImageIcon("./image/다운로드.jpg").getImage());
+		ImagePanel panel = new ImagePanel(new ImageIcon("./image/bird.jpg").getImage());
 		frame.add(panel);
 		frame.pack();
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 
 		
+		/*public class Lesson8 {
+			public static void main(String[] args) {
+				JFrame frame = new JFrame("Lesson 08");
+				frame.setSize(640, 480);
+				frame.setLocationRelativeTo(null);
+				frame.setVisible(true);
+				
+				ImagePanel panel = new ImagePanel(new ImageIcon("./image/bird.jpg").getImage());
+				frame.add(panel);
+				frame.pack();
+				
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				
+		*/
 	}
 
 }
